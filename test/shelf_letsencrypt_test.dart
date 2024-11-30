63b4bf79f2ff1adbbe42356cb5b44319af550534
gmpassos <gracilianomp@gmail.com>
2024-11-30 19:44:26 -0300

    shelf_letsencrypt_test.dart

--- test/shelf_letsencrypt_test.dart
+++ test/shelf_letsencrypt_test.dart
@@ -162,22 +162,43 @@ void main() {
 
       expect(letsEncrypt.production, isFalse);
 
-      StateError? error;
-      try {
-        await letsEncrypt.startServer(
-          (request) => Response.ok('Requested: ${request.requestedUri}'),
-          [const Domain(name: 'localhost', email: 'contact@localhost')],
-          requestCertificate: false,
-        );
-      } catch (e) {
-        error = e as StateError;
+      {
+        ArgumentError? error;
+        try {
+          await letsEncrypt.startServer(
+            (request) => Response.ok('Requested: ${request.requestedUri}'),
+            [const Domain(name: 'localhost', email: 'contact@localhost')],
+            requestCertificate: false,
+          );
+        } catch (e) {
+          error = e as ArgumentError;
+        }
+
+        expect(error, isNotNull);
+        expect(
+            error?.message,
+            allOf(contains("Empty `domains`"),
+                contains("No valid domain provided")));
       }
 
-      expect(error, isNotNull);
-      expect(
-          error?.message,
-          allOf(contains("Can't load all `SecurityContext`s"),
-              contains("can't request")));
+      {
+        StateError? error;
+        try {
+          await letsEncrypt.startServer(
+            (request) => Response.ok('Requested: ${request.requestedUri}'),
+            [const Domain(name: 'example.com', email: 'contact@example.com')],
+            requestCertificate: false,
+          );
+        } catch (e) {
+          error = e as StateError;
+        }
+
+        expect(error, isNotNull);
+        expect(
+            error?.message,
+            allOf(contains("Can't load all `SecurityContext`s"),
+                contains("can't request")));
+      }
     });
 
     tearDown(() {
